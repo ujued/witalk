ab27654cab5d8a41f21211481659b4d7ef6ced44
Ujued <ujued@qq.com>
2018-04-03 16:32:12 +0800

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-configuration = {
-    'redis_pass' : '123456',
-    'redis_host' : '127.0.0.1',
-    'redis_port' : 6379,
-    'mysql_user' : 'root',
-    'mysql_pass' : '123456',
-    'mysql_host' : '127.0.0.1',
-    'mysql_db'   : 'uoope_tech'
-}
